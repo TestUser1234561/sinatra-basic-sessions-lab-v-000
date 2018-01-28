f2d84de2358060c2804618183644f2b385f2841e
Morgan Fudge <mfudge92@gmail.com>
2018-01-28 13:08:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,9 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   configure do
     enable :sessions
-    set :session_secret, "secret"
+    set :session_secret, "secret1"
   end
-  
+
   get '/' do
     erb :index
   end
